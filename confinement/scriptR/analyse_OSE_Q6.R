d78f967d39d081f17726e1e433aff82b736c37c1
Etienne DELAY <abcessus@gmail.com>
2020-04-30 23:04:22 +0200

    finaly use geom_boxplot

--- confinement/scriptR/analyse_OSE_Q6.R
+++ confinement/scriptR/analyse_OSE_Q6.R
@@ -11,6 +11,7 @@
 library(ggplot2)
 library(dplyr)
 library(reshape2)
+library(ggConvexHull)
 
 rm(list = ls())
 
@@ -26,36 +27,52 @@ data.df$nb.R <- data.df$nb.R/200*100
 data.df$s.time <- data.df$X.step. / 4
 
 
-# data.df %>% 
+# data.df %>%
 #   group_by(s.time) %>%
 #   summarise(
 #     nb.S = median(nb.S),
+#     sd.S = sd(nb.S),
 #     nb.Ir = median(nb.Ir),
-#     nb.R = median(nb.R)
+#     sd.Ir = sd(nb.Ir),
+#     nb.R = median(nb.R),
+#     sd.R = sd(nb.R)
 #     ) -> small.df
 
-small.df <- subset(data.df, select =c(s.time,nb.S, nb.Ir, nb.R))
+## preparer le gros data frame pour qu'il soit compatible avec ggplt
+data.dfgg <- subset(data.df, select =c(s.time,nb.S, nb.Ir, nb.R)) %>%
+                melt(id.vars = "s.time")
 
-small.df <- melt(small.df,  id.vars = "s.time")
 
-gg1 <- ggplot(data = small.df)+
-  geom_smooth(aes(x = s.time, y = value, color= variable))+
-  geom_point(aes(x = s.time, y = value, color= variable), alpha = 0.01)+
-  scale_color_manual(values=c("#0099ff", "#ff0000", "#ffffff"), "")+
+## Preparer l'enveloppe des point
+small.df <- subset(data.df, select =c(s.time,nb.S, nb.Ir, nb.R)) %>%
+              group_by(s.time) %>%
+              summarise_each(funs(min, max)) ## Calucler le min et max de chaque tick
+
+small1.df <- small.df[,1:4] %>%
+                melt(id.vars = "s.time") ## Couper par min et convertir en 3 colonnes
+small2.df <- small.df[,c(1,5:7)] %>%
+                melt(id.vars = "s.time") ## Couper par max et convertir en 3 colonnes
+small.df <- small1.df %>% left_join(small2.df, by = "s.time") ## jointure des deux sous data frame
+rm(small1.df, small2.df)
+
+## Plot
+# gg1 <- ggplot()+
+#   geom_point(data = data.dfgg, aes(x = s.time, y = value, color= variable), alpha = 0.05)+
+#   geom_errorbar(data = small.df,aes(x = s.time, y = value.x,
+#                                   ymin = value.x, ymax = value.y,
+#                                   color= variable.x), alpha = 0.01)+
+#   # scale_color_manual(values=c("#0099ff", "#ff0000", "#ffffff"))+
+#   labs(x = "temps", y = "% de la population")
+#   # theme_dark()
+# gg1
+
+gg1 <- ggplot()+
+  geom_boxplot(data = data.dfgg, aes(x = as.factor(s.time), y = value, color= variable), alpha = 0.05)+
+  scale_x_discrete(breaks=seq(0, 127, 20))+
+  scale_color_manual(values=c("#0099ff", "#ff0000", "#ffffff"))+
   labs(x = "temps", y = "% de la population")+
   theme_dark()
 gg1
 
+ggsave("img/out_ose_solution.png",gg1)
 
-data.df %>% 
-  group_by(s.time) %>%
-  summarise(
-    max.Ir = median(max.Ir)
-  ) -> small2.df
-
-
-gg2 <- ggplot(data = data.df)+
-  geom_smooth(aes(x = s.time, y = max.Ir))
-  labs(x = "temps", y = "% de la population max infecté")+
-  theme_dark()
-gg2
