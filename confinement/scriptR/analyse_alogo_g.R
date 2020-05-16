0b2af952be7f2ba311faf8caeca2b4d6f9303b4c
Etienne DELAY <abcessus@gmail.com>
2020-05-16 09:57:48 +0200

    Re-run PSE and plots one fitness once replication proba is understood

--- confinement/scriptR/analyse_alogo_g.R
+++ confinement/scriptR/analyse_alogo_g.R
@@ -1,47 +1,86 @@
 # analyse des sortie de l'ago génétique d'openMole
 #Auteur : E. Delay (CIRAD ES, GREEN)
+## Attention 
+# Dans tout les algo génétique, il ne faut pas prendre toutes les sorties.
+# seulement le dernier fichier générer qui normalement porte les simu
+# les plus prometeuse.
+
 library(ggplot2)
-library(gganimate)
+# library(gganimate)
 
 rm(list = ls())
 
 setwd("~/github/Covprehention/proto/confinement/")
-my.path <- "data/calibration/"
+my.path <- "data/calibration_nsga2/"
+# 
+# files.l <- list.files(my.path)
+# data.df <- data.frame()
+# 
+# ## aggreger les fichers
+# for(i in 1:length(files.l)){
+#   tmp <- read.csv(paste0(my.path, files.l[i]), header = T)
+#   data.df <- rbind(data.df, tmp)
+# }
 
-files.l <- list.files(my.path)
-data.df <- data.frame()
+data.df <- read.csv("data/calibration_nsga2/population20000.csv")
 
-## aggreger les fichers
-for(i in 1:length(files.l)){
-  tmp <- read.csv(paste0(my.path, files.l[i]), header = T)
-  data.df <- rbind(data.df, tmp)
-}
-
+sel <- data.df$evolution.samples >= 100
+data.df <- data.df[sel,]
 summary(data.df)
 
-data.df$nbS <- data.df$nbS/200*100
+
 data.df$pic_max <- data.df$pic_max/200*100
 data.df$init_conf <- data.df$init_conf / 4
 data.df$duration_conf <- data.df$duration_conf / 4
 data.df$relesae_conf <- data.df$relesae_conf / 4
 
-# ggplot(data.df, aes(x = jours_confinement, y = pic_max, colour = nbS)) + 
-#   geom_point()+
-#   transition_states(evolution.generation)+
-#   ease_aes('linear')
 
-colnames(data.df)[4] <- "pause"
-sel <- data.df$pause%%1==0
-
-g1 <- ggplot(data.df[sel,], aes(x = duration_conf, y = init_conf, colour = pic_max)) + 
-        geom_point()+
-        facet_wrap(.~pause, labeller = label_both)+
-        scale_color_gradient("% de pop\nconserné\npar le pic")+
-        labs(title="L'effet de la durée de la pause de confinement\nsur le pic max de contiaminé", 
-             x = "jours de confinements", 
-             y = "premier jours de confinement\n(après le premier cas)")
+g1 <- ggplot() + 
+  geom_point(data = data.df, aes(x = jours_confinement, y = pic_max, size = evolution.samples), alpha = 0.5)+
+  # geom_point(data = small.df, aes(x = duration_conf, y = init_conf, size = -pic_max.1 ,colour = pic_max))+
+  annotate("segment", x = 19, xend = 30, y = 24, yend = 70)+
+  scale_size("Robustesse\ndu résultat")+
+  scale_color_gradient("% de pop\nnon touché par\nle virus")+
+  labs(x = "Nombre de jours de confinement effectif", 
+       y = "Nombre maximum de personne infecté simultanément)")+
+  xlim(0,80)+
+  ylim(10, 100)+
+  theme_bw()
 g1
 
-ggsave("img/ag_explration_1.png", plot = g1 ,width = 15, dpi = 120)
+ggsave("img/Q6-A1-1-NSGA2.png", plot = g1 ,width = 10, height = 8, dpi = 120)
+
+############################################################################################################
+##
+############################################################################################################
 
 
+
+ggplot(data.df, aes(x = duration_conf, y = init_conf, 
+                    colour = pic_max, size = -pic_max.1, group =1) ) + 
+  geom_point()+
+  #facet_wrap(.~relesae_conf, labeller = label_both)+
+  scale_color_gradient("% de pop\nconserné\npar le pic")+
+  scale_size("Ecart type",breaks=c(0,-10,-20, -30), labels=c(0,10,20, 30))+
+  labs(title="Le pic max de contiaminé", 
+       subtitle = 'Frame {frame} of {nframes}',
+       x = "jours de confinements", 
+       y = "premier jours de confinement\n(après le premier cas)")+
+  geom_text(x = 50 , y = 18,  
+            family = "Times",  
+            aes(label = as.character(round(relesae_conf))),  
+            size = 10, col = "grey18")+
+  theme_bw()+
+  transition_time(relesae_conf)
+
+
+ggplot() + 
+  geom_point(data = data.df, aes(x = duration_conf, y = init_conf, colour = pic_max, size = -pic_max.1))+
+  # geom_point(data = small.df, aes(x = duration_conf, y = init_conf, size = -pic_max.1 ,colour = pic_max))+
+  scale_size("Ecart type",breaks=c(0,-10,-20, -30), labels=c(0,10,20, 30))+
+  facet_grid(.~relesae_conf)+
+  scale_color_gradient("% de pop\nconserné\npar le pic")+
+  labs(title="L'effet de la durée de la pause de confinement\nsur le pic max de contiaminé", 
+       x = "nombre de jours de confinements effectif", 
+       y = "premier jours de confinement\n(après le premier cas)")+
+  theme_bw()
--- confinement/scriptR/analyse_pse.R
+++ confinement/scriptR/analyse_pse.R
@@ -1,86 +1,47 @@
 # analyse des sortie de l'ago génétique d'openMole
 # Auteur : E. Delay (CIRAD ES, GREEN)
 library(ggplot2)
-library(gganimate)
-library(gridExtra)
+# library(gganimate)
+#library(gridExtra)
 
 rm(list = ls())
 
 setwd("~/github/Covprehention/proto/confinement/")
-my.path <- "data/results_pse/"
 
-files.l <- list.files(my.path)
-data.df <- data.frame()
-
-## aggreger les fichers
-for(i in 1:length(files.l)){
-  tmp <- read.csv(paste0(my.path, files.l[i]), header = T)
-  data.df <- rbind(data.df, tmp)
-}
+data.df <- read.csv("data/results_pse/population20000.csv", header = T)
 
 summary(data.df)
 
 data.df$nbS <- data.df$nbS/200*100
 data.df$pic_max <- data.df$pic_max/200*100
-data.df$init_conf <- data.df$init_conf / 4
-data.df$duration_conf <- data.df$duration_conf / 4
-data.df$relesae_conf <- data.df$relesae_conf / 4
+data.df$init_conf <- data.df$init_conf / 4         ## input : date du début du premier confinement
+data.df$duration_conf <- data.df$duration_conf / 4 ## input : Durée d'un confinement
+data.df$relesae_conf <- data.df$relesae_conf / 4   ## input : temps entre deux confinement
 
-# ggplot(data.df, aes(x = jours_confinement, y = pic_max, colour = nbS)) + 
-#   geom_point()+
-#   transition_states(evolution.generation)+
-#   ease_aes('linear')
+# output jours_confinement : nombre de jour de confinement effectif
+# output pic_max : nombre de personne toucher simultanenement
 
-# colnames(data.df)[4] <- "pause"
-# sel <- data.df$pause%%1==0
-
-# g1 <- ggplot(data.df ) + 
-#         geom_point(aes(x = duration_conf, y = pic_max , colour = init_conf, size = init_conf), alpha = 0.3)+
-#         scale_color_gradient("1er jour de confinement")+
-#         scale_size("Nombre de jour\nréelement confinés")+
-#         labs(title="Résultat de l'ago PSE",
-#              subtitle = "effets sur les parametres d'entrés",
-#              x = "Durée du confinement imposé", 
-#              y = "Nombre max de cas")+
-#         theme_bw()
-# g1
-# 
-# 
-# ggsave("img/ag_explration_pse.png", plot = g1 ,width = 15, dpi = 120)
-data.df$nb_conf_effectif <- data.df$jours_confinement / data.df$duration_conf
-sel <- data.df$duration_conf <= 20 & data.df$init_conf <= 10
-small.df <- data.df[sel,]
 
 g2 <- ggplot() + 
-  geom_point(data.df,  mapping = aes(x = jours_confinement, y = pic_max), shape = 1, alpha = 0.1)+
-  # geom_smooth(data.df,  mapping = aes(x = jours_confinement, y = pic_max, colour = nbS, size = nbS))+
-  geom_point(small.df,  mapping = aes(x = jours_confinement, y = pic_max, colour = nbS, size = nb_conf_effectif))+
-  # geom_smooth(small.df,  mapping = aes(x = jours_confinement, y = pic_max, colour = nbS, size = nbS))+
-  # facet_wrap(.~pause, labeller = label_both)+
+  geom_point(data.df,  mapping = aes(x = jours_confinement, y = pic_max, colour = nbS, size = evolution.samples))+
+  annotate("segment", x = 26.5, y = 21.5, xend = 50, yend = 10)+
+  annotate("text", x = 54, y = 10, label = "Simu. 1")+
+  annotate("segment", x = 13.5, y = 10, xend = 7, yend = 50)+
+  annotate("text", x = 7, y = 52, label = "Simu. 2")+
   scale_color_gradient("% de pop\nnon touché par\nle virus")+
-  scale_size("nombre de\nconfinement\neffectué")+
-  labs(x = "Nombre de jour de confinement effectif", 
+  scale_size("Robustesse\ndu résultat")+
+  labs(x = "Nombre de jours de confinement effectif", 
        y = "Nombre maximum de personne infecté simultanément")+
+  # xlim(0,80)+
+  # ylim(10, 100)+
   theme_bw()
 g2
 
-g3 <- ggplot() + 
-  geom_point(data.df,  mapping = aes(x = jours_confinement, y = pic_max,
-                                     colour = init_conf, size = nb_conf_effectif))+
-  # geom_smooth(data.df,  mapping = aes(x = jours_confinement, y = pic_max, colour = nbS, size = nbS))+
-  # geom_point(small.df,  mapping = aes(x = jours_confinement, y = pic_max, colour = init_conf, size = nb_conf_effectif))+
-  # geom_smooth(small.df,  mapping = aes(x = jours_confinement, y = pic_max, colour = nbS, size = nbS))+
-  # facet_wrap(.~pause, labeller = label_both)+
-  scale_color_gradient("Date du\nPremier\nConfinement")+
-  scale_size("nombre de\nconfinement\neffectué")+
-  labs(x = "Nombre de jour de confinement effectif", 
-       y = "Nombre maximum de personne infecté simultanément")+
-  theme_bw()
-g3
-
-ggsave("img/Q6-A1-2-PSE.png", plot = g3 ,width = 10, height = 8, dpi = 120)
+ggsave("img/Q6-A1-1-PSE.png", plot = g2 ,width = 10, height = 8, dpi = 120)
 
 
-## Conter le nombre de simulation qui on nb-j-conf/ confinement-duree > 1
+## identifier les paramètres de simulation
+
+
 data.df$nb_conf_effectif <- data.df$jours_confinement / data.df$duration_conf
 length(data.df[data.df$nb_conf_effectif >= 1,1])
