10373f609e039f3316cb315326dc1da9ee771ce9
Etienne D <etienne.delay@cirad.fr>
2020-05-23 21:13:00 +0200

    Clean up output
    remove old images, and update R script for good ones

--- confinement/scriptR/analyse_pse.R
+++ confinement/scriptR/analyse_pse.R
@@ -1,7 +1,7 @@
 # analyse des sortie de l'ago génétique d'openMole
 # Auteur : E. Delay (CIRAD ES, GREEN)
 library(ggplot2)
-# library(gganimate)
+library(gganimate)
 #library(gridExtra)
 
 rm(list = ls())
@@ -17,6 +17,7 @@ data.df$pic_max <- data.df$pic_max/200*100
 data.df$init_conf <- data.df$init_conf / 4         ## input : date du début du premier confinement
 data.df$duration_conf <- data.df$duration_conf / 4 ## input : Durée d'un confinement
 data.df$relesae_conf <- data.df$relesae_conf / 4   ## input : temps entre deux confinement
+data.df$evolution.samples <- data.df$evolution.samples / 200
 
 # output jours_confinement : nombre de jour de confinement effectif
 # output pic_max : nombre de personne toucher simultanenement
@@ -25,14 +26,14 @@ data.df$relesae_conf <- data.df$relesae_conf / 4   ## input : temps entre deux c
 g2 <- ggplot() + 
   geom_point(data.df,  mapping = aes(x = jours_confinement, y = pic_max, colour = nbS, size = evolution.samples))+
   annotate("segment", x = 26.5, y = 21.5, xend = 50, yend = 10)+
-  annotate("text", x = 54, y = 10, label = "Simu. 1")+
+  annotate("text", x = 54, y = 10, label = "A")+
   annotate("segment", x = 13.5, y = 10, xend = 7, yend = 50)+
-  annotate("text", x = 7, y = 52, label = "Simu. 2")+
+  annotate("text", x = 7, y = 53, label = "B")+
   annotate("rect", xmin = 0, xmax = 27, ymin = 0, ymax = 25, alpha = .2)+
   scale_color_gradient("% de pop\nnon touché par\nle virus")+
   scale_size("Robustesse\ndu résultat")+
-  labs(x = "Nombre de jours de confinement effectif", 
-       y = "Nombre maximum de personne infecté simultanément")+
+  labs(x = "Nombre total de jours de confinement pour chaque simulation", 
+       y = "Nombre maximum de personnes infectées simultanément (pic de l’épidémie)")+
   # xlim(0,80)+
   # ylim(10, 100)+
   theme_bw()
@@ -47,3 +48,19 @@ small.df <- data.df[sel,]
 
 data.df$nb_conf_effectif <- data.df$jours_confinement / data.df$duration_conf
 length(data.df[data.df$nb_conf_effectif >= 1,1])
+
+## animation sur la date du premier confinement
+
+g3 <- ggplot() + 
+  geom_point(data.df,  mapping = aes(x = jours_confinement, y = pic_max, colour = nbS, size = evolution.samples))+
+  scale_color_gradient("% de pop\nnon touché par\nle virus")+
+  scale_size("Robustesse\ndu résultat")+
+  labs(x = "Nombre de jours de confinement effectif", 
+       y = "Nombre maximum de personne infecté simultanément",
+       title = 'Premier jour de confinement: {frame_time}')+
+  # xlim(0,80)+
+  # ylim(10, 100)+
+  theme_bw()+
+  transition_time(init_conf)
+
+animate(g3, renderer = ffmpeg_renderer())
